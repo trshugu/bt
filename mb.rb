58a4080a14015cdb463dcdfe4c995f1504dfaa01
trser <trser@terijoa.eon>
2012-07-25 16:49:39 +0900

    marobot ver1.0

---
+++ mb.rb
@@ -0,0 +1,21 @@
+#!ruby
+# -*- coding: utf-8 -*-
+require 'twitter'
+
+lineArray = Array.new()
+File.readlines("oauth.txt").each do |line|
+  lineArray.push line
+end
+
+Twitter.configure do |c|
+  c.consumer_key = lineArray[0].chomp
+  c.consumer_secret = lineArray[1].chomp
+  c.oauth_token = lineArray[2].chomp
+  c.oauth_token_secret = lineArray[3].chomp
+end
+
+tweet = "test"
+loop do
+  Twitter.update(tweet)
+  sleep 43200
+end
