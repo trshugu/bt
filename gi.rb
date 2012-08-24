363db9e125dbbae4cb104747a9399d69967b261e
trser <trser@terijoa.eon>
2012-08-24 17:02:44 +0900

    一応コミト

--- gi.rb
+++ gi.rb
@@ -1,11 +1,13 @@
 commands = [
 "gem install bundle --no-ri --no-rdoc",
 "gem install twitter --no-ri --no-rdoc",
+"gem install rake --no-ri --no-rdoc",
 "gem install rspec --no-ri --no-rdoc",
 "gem install rails --no-ri --no-rdoc",
 "gem install cucumber --no-ri --no-rdoc",
 "gem install pik --no-ri --no-rdoc",
 "gem install sinatra --no-ri --no-rdoc",
+"gem install capistrano --no-ri --no-rdoc",
 "gem install atomutil --no-ri --no-rdoc"
 ]
 
