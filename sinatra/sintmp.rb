e110affb50861ad267deebf2db2c8c9767635814
trser <trser@terijoa.eon>
2012-08-28 14:08:04 +0900

    要件定義書追加

---
+++ sinatra/sintmp.rb
@@ -0,0 +1,21 @@
+#!ruby
+# -*- coding: utf-8 -*-
+require 'sinatra'
+=begin
+helpers do
+  include Rack::Utils; alias_method :h, :escape_html
+end
+
+template :layout do
+  "<html><body><h1>Hello World</h1><%= yield %></body></html>"
+end
+=end
+
+=begin
+=end
+
+get '/' do
+end
+
+post '/ do
+end
