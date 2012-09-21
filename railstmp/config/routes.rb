f8f68104629ec548e26689831c0e4d3ce23fced2
trser <trser@terijoa.eon>
2012-09-21 16:44:22 +0900

    jQueryTmpを作成。

--- railstmp/config/routes.rb
+++ railstmp/config/routes.rb
@@ -1,6 +1,9 @@
 Railstmp::Application.routes.draw do
   resources :tmps
 
+  root :to => 'tmps#index'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
