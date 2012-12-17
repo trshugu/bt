1f8aa9180818a61ed02169554fafa33000fe87fb
arr <att@atatjia.comcom>
2012-12-17 18:30:42 +0900

    routes.rbでresourcesを使わずに同じものを生成した

--- railstmp/config/routes.rb
+++ railstmp/config/routes.rb
@@ -15,13 +15,19 @@
   # ano  /tmps(.:format) tmps#index
   #match "tmps" => "tmps#index", :as => "ano"
   
-  get "tmps" => "tmps#index", :as => "ano"
-  post "tmps" => "tmps#index", :as => "ano"
+  get "tmps" => "tmps#index", :as => "tmps"
+  post "tmps" => "tmps#create"
+  get "tmps/new" => "tmps#new", :as => "new_tmp"
+  get "tmps/:id/edit" => "tmps#edit", :as => "edit_tmp"
+  get "tmps/:id" => "tmps#show", :as => "tmp"
+  put "tmps/:id" => "tmps#update"
+  delete "tmps/:id" => "tmps#destroy"
   
   root :to => 'tmps#index'
 end
 
 =begin
+# resources
     tmps GET    /tmps(.:format)          tmps#index
          POST   /tmps(.:format)          tmps#create
  new_tmp GET    /tmps/new(.:format)      tmps#new
@@ -31,3 +37,19 @@ edit_tmp GET    /tmps/:id/edit(.:format) tmps#edit
          DELETE /tmps/:id(.:format)      tmps#destroy
     root        /                        tmps#index
 =end
+
+=begin
+# てづくり
+    tmps GET    /tmps(.:format)          tmps#index
+         POST   /tmps(.:format)          tmps#create
+ new_tmp GET    /tmps/new(.:format)      tmps#new
+edit_tmp GET    /tmps/:id/edit(.:format) tmps#edit
+     tmp GET    /tmps/:id(.:format)      tmps#show
+         PUT    /tmps/:id(.:format)      tmps#update
+         DELETE /tmps/:id(.:format)      tmps#destroy
+    root        /                        tmps#index
+=end
+
+
+
+
