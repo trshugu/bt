ef01bde0060541e8017ef8f8185be54c065c4ef7
rtrs <a@a.a>
2013-05-10 18:59:17 +0900

    'testin'

--- tmp.rb
+++ tmp.rb
@@ -10,7 +10,6 @@
 
 
 
-
 =begin
 # CouchbaseのAPIあれこれ
 require "json"
@@ -18,8 +17,9 @@ require "net/http"
 
 #uri = "http://localhost:8092/"
 uri = "http://localhost:8092/beer-sample/_design/beer/_view/brewery_beers/"
-res = Net::HTTP.get_response(URI.parse(uri))
+#uri = "http://192.168.80.64:8092/beer-sample/_design/beer/_view/brewery_beers/"
 starttime = Time.now
+res = Net::HTTP.get_response(URI.parse(uri))
 puts JSON.parse(res.body)["rows"][0]["key"][0]
 endtime = Time.now
 puts endtime - starttime
