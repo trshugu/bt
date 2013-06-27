0d4fa9f86cec8244d6ce734ac421bb16692ad029
rtrs <a@a.a>
2013-06-27 18:40:48 +0900

    20130627

---
+++ craft/tmpcapybara/features/step_definitions/steps.rb
@@ -0,0 +1,16 @@
+﻿# coding: utf-8
+require 'capybara'
+require 'capybara/dsl'
+require 'capybara-webkit'
+
+Capybara.default_driver = :webkit
+
+include Capybara::DSL
+
+前提 /\"(.+)\"を表示/ do |url|
+  visit(url)
+end
+
+ならば /スクリーンショットを取得し\"(.+)\"に保存/ do |filename|
+  page.save_screenshot "./#{filename}"
+end
