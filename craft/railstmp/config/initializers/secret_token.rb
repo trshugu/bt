66db116a0c19535f3700a5c6a602effbe5be9a93
rtrs <a@a.a>
2013-12-16 17:24:13 +0900

    gitpush2013/12/16

---
+++ craft/railstmp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstmp::Application.config.secret_token = 'd01556fe64b48b1e5434a978d1e5ee657442b496ed2b307d5a129265072489073cfc2bcc4d4a3d1232486c4f4d3fced6a27ae863905e157845af5d752ec3b804'
