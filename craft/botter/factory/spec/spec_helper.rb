2afaaedf0ccbc6ba2c4b9b2ddbb8e5b5db1b36fa
rtrs <a@a.a>
2014-03-10 15:54:52 +0900

    gitpush2014/03/10

---
+++ craft/botter/factory/spec/spec_helper.rb
@@ -0,0 +1,9 @@
+﻿# coding: utf-8
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..')) # 対象のdir
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'bot')) # 対象のdir
+
+require 'rspec'
+require 'bot' # 対象のクラス
+require 'assets/betweenhour' # 対象のクラス
