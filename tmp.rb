2f416f9fdf672338f0932f82936b1346991ee3af
rtrs <a@a.a>
2014-03-26 13:28:37 +0900

    gitpush2014/03/26

--- tmp.rb
+++ tmp.rb
@@ -12,6 +12,34 @@
 
 
 =begin
+# rubyでPOST
+require 'uri'
+require 'net/http'
+
+def http_request(method, uri, query_hash={})
+  query = query_hash.map{|k, v| "#{k}=#{v}"}.join('&')        #ハッシュをオプションの書式に変換
+  query_escaped = URI.escape(query)
+  
+  uri_parsed = URI.parse(uri)
+  http = Net::HTTP.new(uri_parsed.host)
+  
+  case method.downcase!
+  when 'get'
+    return http.get(uri_parsed.path + '?' + query_escaped).body
+  when 'post'
+    return http.post(uri_parsed.path, query_escaped).body
+  end
+end
+ 
+puts http_request('POST', 'http://posporpor', {:hoge => '20537'})
+=end
+
+
+
+
+
+
+=begin
 # rubyでenum風
 module Status
   REQUEST = 1
