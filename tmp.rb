ac1b778d9f08e4e2f5ff417cef9eb5afdb9dbdeb
rtrs <a@a.a>
2014-04-21 10:34:42 +0900

    gitpush2014/04/21

--- tmp.rb
+++ tmp.rb
@@ -6,6 +6,42 @@
 
 
 
+
+=begin
+# sequel + logger
+require "sequel"
+require "logger"
+
+# connect to an in-memory database
+DB = Sequel.sqlite("tmp.db", :loggers => [Logger.new($stdout)])
+#DB = Sequel.connect('postgres://user:password@localhost/my_db')
+#DB = Sequel.postgres('my_db', :user => 'user', :password => 'password', :host => 'localhost')
+#DB = Sequel.ado('mydb')
+
+# create an items table
+DB.create_table :items do
+  primary_key :id
+  String :name
+  Float :price
+end
+
+# create a dataset from the items table
+items = DB[:items]
+
+# populate the table
+items.insert(:name => 'abc', :price => rand * 100)
+items.insert(:name => 'def', :price => rand * 100)
+items.insert(:name => 'ghi', :price => rand * 100)
+
+# print out the number of records
+puts "Item count: #{items.count}"
+
+# print out the average price
+puts "The average price is: #{items.avg(:price)}"
+=end
+
+
+
 =begin
 # 標準入力を標準出力へ
 puts gets
